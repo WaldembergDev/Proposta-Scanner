a639be018e94aa316e840a48c164e07eb526e990
Waldemberg Pereira <waldemberg.pereira@outlook.com>
2025-03-26 10:46:33 -0300

    Criado a função que encontra pasta e retorna o caminho da pasta se ela existir

--- aplicacao_parcial.py
+++ aplicacao_parcial.py
@@ -1,13 +1,10 @@
-from pathlib import Path
 import unicodedata
 import re
 import pandas as pd
 from src.controller.Controller import ControllerConsulta
 from datetime import datetime, timedelta
-
-from src.controller.Controller import ControllerConsulta
 from src.services import conexao_db
-from datetime import datetime, timedelta
+from pathlib import Path
 
 lista_teste = [
   (3643, 2025, 'ACX CONTROLE DE PRAGAS LTDA', 0, 'Aprovado', 'Comercial 7'),
@@ -17,15 +14,40 @@ lista_teste = [
   (3647, 2025, 'RS RODRIGUES DESENTUPIDORA E DEDETIZADORA LTDA', 0, 'Aprovado', 'Comercial 7'),
   (3648, 2025, 'RS RODRIGUES DESENTUPIDORA E DEDETIZADORA LTDA', 0, 'Aprovado', 'Comercial 7'),
   (3649, 2025, 'APS SOLUCOES COMERCIO E SERVICO LTDA', 0, 'Aprovado', 'Comercial 7'),
-  (3650, 2025, 'THELIO MOREIRA LIMA', 0, 'Em aprovação', 'Comercial 1')
+  (3650, 2025, 'THELIO MOREIRA LIMA', 0, 'Em aprovação', 'Comercial 1'),
+  (3730, 2025, 'THELIO MOREIRA LIMA', 0, 'Aprovado', 'Comercial 1'),
+  (3354, 2025, 'THELIO MOREIRA LIMA', 0, 'Aprovado', 'Comercial 1'),
 ]
 
 # Caminho dos arquivos
 caminho = Path(r'Z:\QUALYLAB\Q-COMERCIAL')
 
-# verificar se existe pasta
-for os in lista_teste:
-  # casos que deverão ser ignorados
-  if os[5] == 'Comercial 7' or os[4] == 'Em aprovação' or os[4] == 'Em análise crítica':
-    continue
+# verificando se a pasta da proposta existe
+def encontrar_pasta(ano: int, os: int):
+  caminho = Path(r'Z:\QUALYLAB\Q-COMERCIAL')
+  caminho_ano = caminho / f'OS {ano}'
+  for pasta in caminho_ano.iterdir():
+    OSs_pasta = ControllerConsulta.obter_numeros(pasta.name)
+    if os in OSs_pasta:
+      return pasta
+  return False
+
+# for resultado in lista_teste:
+#   # caresultados que deverão ser ignorados
+#   if resultado[5] == 'Comercial 7' \
+#     or resultado[4] == 'Em aprovação' \
+#       or resultado[4] == 'Em análise crítica':
+#     continue
+#   # definir os arquivos/pastas que deverão ser procurados
+#   arquivos_padroes = [False, False] # geral - solic, prop
+#   revisoes = [[False, False] for i in range(resultado[3])] # proposta, solicitação
+#   if resultado[0] != 'C&M SERVIÇOS AMBIENTAIS':
+#     arquivos_qualy = [False, False] # Caso qualylab - aprovação, condições comerciais
+#   # Salvando os dados em uma variável mais amigável
+#   ano = resultado[1]
+#   os = resultado[0]
+#   pasta_existe = encontrar_pasta(ano, os)
+#   print(f'OS: {os} - {pasta_existe}')
+    
+  
   
